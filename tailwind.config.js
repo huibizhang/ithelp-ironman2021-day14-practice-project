3805584501a60fe7f7f0ef0e21beec95004b1da6
搋兔 <naruto6404@hotmail.com>
2021-08-25 15:54:20 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      "123": ""
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
